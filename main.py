34b675cf478b1ae9724fd10bfda79512fb7001c8
AshwinDEM <ashwindharmavaram@gmail.com>
2025-02-02 22:32:16 +0530

    Made changes to streamlit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from keras.models import load_model
 import tensorflow as tf
-
 tf.config.set_visible_devices([], 'GPU')
 import logging
 
@@ -12,7 +11,9 @@ st.title("Engine Maintenance")
 
 file = st.file_uploader("Enter your data file")
 
-model = load_model("models/model.keras")
+binary_model = load_model("models/model.keras")
+
+rul_model = load_model("models/model.keras")
 
 sequence_length = 50
 num_features = 24
@@ -20,11 +21,17 @@ num_features = 24
 if file is not None:
     df = pd.read_csv(file)
     st.write(df)
-    random_data = df.to_numpy().reshape(1, sequence_length, num_features)
+    data= df.to_numpy().reshape(1, sequence_length, num_features)
 
-    prediction = model.predict(random_data)
+    prediction = binary_model.predict(data)
     prediction = prediction[0][0]
-    if round(prediction):
-        st.title("The engine is not damaged")
-    else:
-        st.title("The engine is damaged")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        if round(prediction):
+            st.title("The engine is not damaged")
+        else:
+            st.title("The engine is damaged")
+    with col2:
+        RUL = rul_model.predict(data)
+        st.title(f"The number of cycles left = {round(1 / RUL[0][0])}")
